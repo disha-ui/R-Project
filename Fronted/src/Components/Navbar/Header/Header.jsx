685db147c4fe8dde9a12ec696ca4ed1ba984da9d
disha-ui <dishapatel.alphabit@gmail.com>
2024-06-28 16:00:22 +0530

    first commit

---
+++ Fronted/src/Components/Navbar/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis aliquam animi voluptatibus porro a rerum nihil nesciunt. Perspiciatis, sapiente adipisci. Illo corrupti at dolorum nostrum nisi officiis dolor blanditiis aliquam!</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
